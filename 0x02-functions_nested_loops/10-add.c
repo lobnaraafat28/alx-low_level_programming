1141adad23d831db6b59c61c912f9d7b48605d2a
lobnaraafat28 <lobnaraafat23@gmail.com>
2023-07-31 04:56:43 -0700

    Ready for check

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add- add two numbers
+ *@x: the frist pram
+ *@y: the second pram
+ *
+ * Return: the addation of two numbers
+ *
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}

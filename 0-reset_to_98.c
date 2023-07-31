1488a906524a0555bec1bf55a26bb525d5317ae7
lobnaraafat28 <lobnaraafat23@gmail.com>
2023-07-31 05:39:18 -0700

    Ready for check

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value to 98
+ *
+ *@n: pointer to address of the input variable
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

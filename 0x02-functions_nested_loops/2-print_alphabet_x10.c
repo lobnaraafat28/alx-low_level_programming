66821eaf969312274d4022762f57ca1cd86837e6
lobnaraafat28 <lobnaraafat23@gmail.com>
2023-07-31 04:39:59 -0700

    Ready for check

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the lower alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
